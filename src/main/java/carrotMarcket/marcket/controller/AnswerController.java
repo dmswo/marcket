32b7f07cda800690b258efb88ffe387ecc177d73
dmswo106 <dmswo106@naver.com>
2023-08-30 16:55:46 +0900

    젠킨스 깃헙 연동을 위한 테스트커밋

--- src/main/java/carrotMarcket/marcket/controller/AnswerController.java
+++ src/main/java/carrotMarcket/marcket/controller/AnswerController.java
@@ -18,7 +18,7 @@ public class AnswerController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello1111";
+        return "Hello11111";
     }
 
     @PostMapping("/save")
