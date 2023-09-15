cd48e6663268c15bbbe7c8a983e81938d93bc388
dmswo106 <dmswo106@naver.com>
2023-09-15 22:10:56 +0900

    빌드안되서 테스트 주석처리해제3

--- src/test/java/carrotMarcket/marcket/service/AnswerServiceTest.java
+++ src/test/java/carrotMarcket/marcket/service/AnswerServiceTest.java
@@ -36,54 +36,54 @@ class AnswerServiceTest {
         questionRepository.deleteAll();
     }
 
-//    @Test
-//    @DisplayName("답변 작성")
-//    public void save() {
-//        //given
-//        QuestionSaveDto questionSave = QuestionSaveDto.builder()
-//                .title("title")
-//                .text("text")
-//                .build();
-//        Long save = questionService.save(questionSave);
-//
-//        AnswerSaveDto answerSave = AnswerSaveDto.builder()
-//                .text("reply1")
-//                .questionId(save)
-//                .build();
-//        answerService.save(answerSave);
-//
-//        //when
-//
-//        //then
-//        Assertions.assertEquals(1L, answerRepository.count());
-//        Answer answer = answerRepository.findAll().get(0);
-//        Assertions.assertEquals("reply1", answer.getText());
-//        Assertions.assertEquals(save, answer.getId());
-//    }
+    @Test
+    @DisplayName("답변 작성")
+    public void save() {
+        //given
+        QuestionSaveDto questionSave = QuestionSaveDto.builder()
+                .title("title")
+                .text("text")
+                .build();
+        Long save = questionService.save(questionSave);
 
-//    @Test
-//    @DisplayName("답변 삭제")
-//    public void delete() {
-//        //given
-//        QuestionSaveDto save = QuestionSaveDto.builder()
-//                .text("text")
-//                .title("title")
-//                .build();
-//
-//        AnswerSaveDto answerSave = AnswerSaveDto.builder()
-//                .text("reply")
-//                .questionId(1L)
-//                .build();
-//
-//        //when
-//        questionService.save(save);
-//        answerService.save(answerSave);
-//
-//        Answer answer = answerRepository.findAll().get(0);
-//        answerService.deleteById(answer.getId());
-//
-//        //then
-//        Assertions.assertEquals(0L, answerRepository.count());
-//    }
+        AnswerSaveDto answerSave = AnswerSaveDto.builder()
+                .text("reply1")
+                .questionId(save)
+                .build();
+        answerService.save(answerSave);
+
+        //when
+
+        //then
+        Assertions.assertEquals(1L, answerRepository.count());
+        Answer answer = answerRepository.findAll().get(0);
+        Assertions.assertEquals("reply1", answer.getText());
+        Assertions.assertEquals(save, answer.getId());
+    }
+
+    @Test
+    @DisplayName("답변 삭제")
+    public void delete() {
+        //given
+        QuestionSaveDto save = QuestionSaveDto.builder()
+                .text("text")
+                .title("title")
+                .build();
+
+        AnswerSaveDto answerSave = AnswerSaveDto.builder()
+                .text("reply")
+                .questionId(1L)
+                .build();
+
+        //when
+        questionService.save(save);
+        answerService.save(answerSave);
+
+        Answer answer = answerRepository.findAll().get(0);
+        answerService.deleteById(answer.getId());
+
+        //then
+        Assertions.assertEquals(0L, answerRepository.count());
+    }
 
 }
