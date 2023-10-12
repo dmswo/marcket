36bff95ebbac5da73efb9cd18d35be315be9a2df
dmswo106 <dmswo106@naver.com>
2023-10-12 18:46:06 +0900

    토큰이 만료되서 토큰 갱신후 자동 빌드 테스트

--- src/main/java/carrotMarcket/marcket/controller/AnswerController.java
+++ src/main/java/carrotMarcket/marcket/controller/AnswerController.java
@@ -18,7 +18,7 @@ public class AnswerController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello111";
+        return "Hello_token";
     }
 
     @PostMapping("/save")
