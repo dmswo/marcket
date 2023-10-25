36907e27267628be1c70a91b027bef25d7572d8d
dmswo106 <dmswo106@naver.com>
2023-10-25 23:29:25 +0900

    nginx 테스트

--- src/main/java/carrotMarcket/marcket/config/WebConfig.java
+++ src/main/java/carrotMarcket/marcket/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedOrigins("http://localhost:8080")
                 .allowedMethods(HttpMethod.GET.name());
     }
 }
--- src/main/java/carrotMarcket/marcket/controller/profileController.java
+++ src/main/java/carrotMarcket/marcket/controller/profileController.java
@@ -2,17 +2,21 @@ package carrotMarcket.marcket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
-@Controller
-public class TestController {
+@RestController
+public class profileController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "dmswo";
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
