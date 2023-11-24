25d5916520b54004aaa42f3286167d9c55e6b79e
dmswo106 <dmswo106@naver.com>
2023-11-24 20:41:56 +0900

    import 제거

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into Users (login_id, password, nickname, role) values ('123', '123', '123', 'USER');
-insert into Board (title, text, board_status) values ('title1', 'text1', 'WAIT');
-insert into Board (title, text, board_status) values ('title2', 'text2', 'WAIT');
+--insert into Users (login_id, password, nickname, role) values ('123', '123', '123', 'USER');
+--insert into Board (title, text, board_status) values ('title1', 'text1', 'WAIT');
+--insert into Board (title, text, board_status) values ('title2', 'text2', 'WAIT');
