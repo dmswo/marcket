7ca8671cbb447942a68043ac4b90ec6fd487ae32
dmswo106 <dmswo106@naver.com>
2023-12-18 02:40:07 +0900

    jpa AuditorAwareImpl 추가

---
+++ src/main/java/carrotMarcket/marcket/global/config/AuditorAwareImpl.java
@@ -0,0 +1,21 @@
+package carrotMarcket.marcket.global.config;
+
+import carrotMarcket.marcket.user.entity.Users;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        User principal = (User) authentication.getPrincipal();
+        return Optional.of(principal.getUsername());
+    }
+}
---
+++ src/main/java/carrotMarcket/marcket/global/config/JpaAuditConfig.java
@@ -0,0 +1,13 @@
+package carrotMarcket.marcket.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+@Configuration
+public class JpaAuditConfig {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+}
